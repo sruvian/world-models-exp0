f0873cd218cb344f88528e5cd3fe59d98186c25a
sruvian <srujan.sardeshmukh@gmail.com>
2026-07-01 17:03:28 +0530

    added models to make_model

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .simplenn import SimpleNN
 from .model import make_model
-from .wmodel import WorldModel
+from .wmodel import WorldModel, WorldModelVAE, WorldModelDMD, WorldModelGRU
 from .transfer import ProtocolAModel, ProtocolBModel
 
 __all__ = [
--- models/model.py
+++ models/model.py
@@ -1,7 +1,7 @@
 from models.transfer import ProtocolBModel, ProtocolAModel
 
 from .simplenn import SimpleNN
-from .wmodel import WorldModel
+from .wmodel import WorldModel, WorldModelDMD, WorldModelGRU
 from .wmodel import WorldModelVAE
 
 def make_model(model_name, **kwargs):
@@ -11,5 +11,12 @@ def make_model(model_name, **kwargs):
     
     elif model_name == "WorldModelVAE":
         return WorldModelVAE(SimpleNN, **kwargs)
+    
+    elif model_name == 'WorldModelDMD':
+        return WorldModelDMD(SimpleNN, **kwargs)
+    
+    elif model_name == "WorldModelGRU":
+        return WorldModelGRU(SimpleNN, **kwargs)
+    
     else:
         raise ValueError("Model Unavailable")
